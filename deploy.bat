8947147e0d50c979e2fddf21974a12c12d3d7458
Curtis Sapp <curtmsapp@gmail.com>
2021-09-02 06:55:04 -0700

    Remove escaped strings from deploy

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'SilentlyContinue'
 type nul >> "out/.nojekyll"
 git add out/ 
-git commit -m \"Deploy Next.js to remote\"
+git commit -m "Deploy Next.js to remote"
 git subtree push --prefix out origin gh-pages
