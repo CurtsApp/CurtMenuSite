3dbae266d949e49c743614703e01cd0393fb60c8
Curtis Sapp <curtmsapp@gmail.com>
2021-09-02 06:57:08 -0700

    Remove /out from ignore

---
+++ out/_next/static/-5Y57p0NcyP0BULCrmHxO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-230ea3c93863f9f66750.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
