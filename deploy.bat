63107dd63c728492d37395aaab5b7abeebc2f0ff
Curtis Sapp <curtmsapp@gmail.com>
2021-09-02 19:43:23 -0700

    Deploy now resets branch

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,10 @@
 $ErrorActionPreference = 'SilentlyContinue'
+git stash
 type nul >> "out/.nojekyll"
 git add out/ 
 git commit -m "Deploy Next.js to remote"
-::git subtree push --force --prefix out origin gh-pages
-git subtree split --prefix dist -b gh-pages
+git subtree split --prefix out -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
+git reset --hard HEAD~1
+git stash pop
