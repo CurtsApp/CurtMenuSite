68b487dbca40cdd418b4df639a2044b213cc45d5
Curtis Sapp <curtmsapp@gmail.com>
2021-09-02 19:23:06 -0700

    Verbose forcefulness

--- deploy.bat
+++ deploy.bat
@@ -3,4 +3,6 @@ type nul >> "out/.nojekyll"
 git add out/ 
 git commit -m "Deploy Next.js to remote"
 ::git subtree push --force --prefix out origin gh-pages
-git push origin "git subtree split --prefix out master":gh-pages --force
+git subtree split --prefix dist -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
