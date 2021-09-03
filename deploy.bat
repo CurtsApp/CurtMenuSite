d8b2ac567338ac59e9a69ce1e889b619048d122d
Curtis Sapp <curtmsapp@gmail.com>
2021-09-02 19:20:48 -0700

    Extra force?

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,5 @@ $ErrorActionPreference = 'SilentlyContinue'
 type nul >> "out/.nojekyll"
 git add out/ 
 git commit -m "Deploy Next.js to remote"
-git subtree push --prefix out origin gh-pages --force
+::git subtree push --force --prefix out origin gh-pages
+git push origin "git subtree split --prefix out master":gh-pages --force
