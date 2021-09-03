1cf9b653e07a5cef59be6ef40c586f6c1b269688
Curtis Sapp <curtmsapp@gmail.com>
2021-09-03 05:46:00 -0700

    Added success explanation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
- const nextConfig = {
+// This is needed to keep css ans js href links from breaking locally and on github pages
+const nextConfig = {
     /* config options here */
     assetPrefix: "."
-  }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
